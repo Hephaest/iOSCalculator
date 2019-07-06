a6950ee74889973e99e3fd5270d1ec65efc1c24c
Hephaest <philliphily@gmail.com>
2019-07-06 11:16:10 -0500

    Second Update

--- js_caculator/end_console.js
+++ js_caculator/end_console.js
@@ -1,13 +1,29 @@
-// In case of input error
+/**
+ * Only use pure JavaScript.
+ * @author Hephaest
+ * @version 2018/10/20
+ */
+
+/**
+ * In case of input error.
+ * @param text User input.
+ * @returns {*} the check result.
+ */
 function handle_undefined(text){
     var check = (parseInt(text)) * 0.01;
-    text = (toString(check)!= "object undefined")? check : "Error";
+    text = toString(check)!== "object undefined"? check : "Error";
     return text;
 }
-//In case of input error
+
+/**
+ * In case of input error.
+ * @param text Output.
+ * @returns {any} the output result.
+ */
 function calculator(text){
     var output;
     try{
+        // If the string represents an expression, eval() evaluates the expression.
         output = eval(text);
     } catch(e){
         output= "Error";
@@ -15,53 +31,37 @@ function calculator(text){
     return output;
 }
 
+/**
+ * Traverse the buttons and get the real-time expression.
+ */
 window.onload = function () {
-    var button = document.getElementsByClassName("btn");
-    var expression = document.getElementsByClassName("output_txt")[0];
-    var special_op = document.getElementsByClassName("gray_btn");
-    var record = false;
-    for (var i = 0; i < special_op.length; i++) {
+    let button = document.getElementsByClassName("btn");
+    let expression = document.getElementsByClassName("output_txt")[0];
+    let special_op = document.getElementsByClassName("gray_btn");
+    let record = false;
+    for (let i = 0; i < special_op.length; i++) {
         special_op[i].onclick = function () {
-            if (this.value === "AC") {
-                expression.value = "0";
-            }
-            else if (this.value === "+/-") {
-                expression.value = parseInt(expression.value)* (-1);
-            }
-            else {
-                    expression.value = handle_undefined(expression.value);
-
-                }
+            if (this.value === "AC") expression.value = "0";
+            else if (this.value === "+/-") expression.value = parseInt(expression.value)* (-1);
+            else expression.value = handle_undefined(expression.value);
             }
     }
-    for (var i = 0; i < button.length; i++) {
+
+    for (let i = 0; i < button.length; i++) {
         button[i].onclick = function () {
             if (this.value === "."){
-                if (expression.value.indexOf(".") != -1) {
-                    //do nothing
-                }
-                else {
-                    expression.value += this.value;
-                }
-            }
-            else if (this.value === "÷" || this.value === "×"){
-                if(this.value === "÷"){
-                    expression.value += "/";
-                }
-                else {
-                    expression.value += "*";
-                }
-            }
-            else if(this.value === "="){
+                if (expression.value.indexOf(".") === -1) expression.value += this.value;
+            } else if (this.value === "÷" || this.value === "×"){
+                if(this.value === "÷") expression.value += "/";
+                else expression.value += "*";
+            } else if(this.value === "="){
                 expression.value = calculator(expression.value);
-                record=true;
-            }
-            else {
+                record = true;
+            } else {
                 if(expression.value === "0" || record === true){
                     expression.value = this.value;
                     record = false;
-                }
-                else{
+                } else{
                     expression.value += this.value;
                 }
             }
--- js_caculator/jQuery_solution.js
+++ js_caculator/jQuery_solution.js
@@ -1,11 +1,22 @@
-// In case of input error
+/**
+ * Use pure JQuery.
+ * @author Hephaest
+ * @version 2018/10/20
+ */
+
+/**
+ * Check input and output expression.
+ * @type {{handle_undefined: (function(*=): *), calculator: (function(*=): any)}}
+ */
 $.func={
+    // If input is invalid, display "Error" in text box.
     handle_undefined: function (text) {
         var check = (parseInt(text)) * 0.01;
         text = (toString(check)!= "object undefined")? check : "Error";
         return text;
     },
 
+    // If expression is invalid, display "Error" in text box.
     calculator: function (text) {
         var output;
         try{
@@ -18,11 +29,16 @@ $.func={
 
 }
 
+/**
+ * Check the expression and display the final result.
+ */
 $(document).ready(function () {
     var button = $(".btn");
     var expression = $(".output_txt")[0];
     var special_op = $(".gray_btn");
     var record = false;
+
+    // Check special operators.
     special_op.bind("click",function () {
         if (this.value === "AC") {
             expression.value = "0";
@@ -36,33 +52,21 @@ $(document).ready(function () {
         }
     })
 
+    // Concatenate the expression and display the final result.
     button.bind("click", function () {
         if (this.value === "."){
-            if (expression.value.indexOf(".") != -1) {
-                //do nothing
-            }
-            else {
-                expression.value += this.value;
-            }
-        }
-        else if (this.value === "÷" || this.value === "×"){
-            if(this.value === "÷"){
-                expression.value += "/";
-            }
-            else {
-                expression.value += "*";
-            }
-        }
-        else if(this.value === "="){
+            if (expression.value.indexOf(".") === -1) expression.value += this.value;
+        } else if (this.value === "÷" || this.value === "×"){
+            if(this.value === "÷") expression.value += "/";
+            else expression.value += "*";
+        } else if(this.value === "="){
             expression.value = $.func.calculator(expression.value);
             record=true;
-        }
-        else {
+        } else {
             if(expression.value === "0" || record === true){
                 expression.value = this.value;
                 record = false;
-            }
-            else{
+            } else{
                 expression.value += this.value;
             }
         }
